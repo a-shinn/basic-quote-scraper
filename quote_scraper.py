ddb7e79f3b8b9473732e1215503861f037c59df7
Austin Shinn <81658373+shinnsplints@users.noreply.github.com>
2023-06-02 16:34:45 -0500

    Add files via upload

---
+++ quote_scraper.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pandas as pd
+import bs4 as bs
+import requests
+
+# scrapes quotes, authors, and tags for each entry in the given webpage and creates a dataframe from the results.
+
+webpage = requests.get('https://quotes.toscrape.com/tag/books/')
+soup = bs.BeautifulSoup(webpage.text, 'html.parser')
+quotes = soup.findAll('span', attrs={'class':'text'})
+authors = soup.findAll('small', attrs={'class':'author'})
+tags = soup.findAll('div', attrs={'class':'tags'})
+# login = soup.findAll('div', attrs={'class':'col-md-4'})
+
+# combines quotes,authors,tags into a list then converts into a dataframe
+list1 = [[author.text,quote.text,tags.text] for author,quote,tags in zip(authors,quotes,tags)]
+data = pd.DataFrame(list1,columns=['Author','Quote','Tags'],index=range(1,11))
+
+# cleaning up the tags column
+data['Tags'] = data['Tags'].str.split()
+for tags in data['Tags']:
+    tags = tags.remove('Tags:')
+
+# print(data.sort_values('Author'))
+# print()
+# print(data.describe())
+#
+# data.to_csv('src/quotes.csv', index=False)
+
+# counts frequency of each tag in the list of books
+tag_counts = {}
+for series in data['Tags']:
+    for taglist in series:
+        # tag_counts[taglist] += 1
+        if taglist in tag_counts:
+            tag_counts[taglist] += 1
+        else:
+            tag_counts[taglist] = 1
+
